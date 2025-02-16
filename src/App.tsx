d702158cf0d6b146a7f01a795e74dbda9a834392
chenfz <13277424603@163.com>
2025-02-16 12:18:07 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useState } from "react";
+import './App.css';
+
+function App() {
+  const [num, setNum] = useState(1);
+
+  return (
+    <div onClick={() => setNum(num + 1)}>{num}</div>
+  );
+}
+
+export default App;
